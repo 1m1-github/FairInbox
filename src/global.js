a98a9a82adfe9c798684bd0bd009f8e25cce3983
1m1 <email@1m1.io>
2023-05-04 18:22:57 -0600

    shuffle

--- src/global.js
+++ src/global.js
@@ -74,3 +74,10 @@ export async function sign_and_send(txns, signer) {
         console.log("cancel", error);
     }
 }
+
+export function shuffle(array) {
+for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+}
+}
